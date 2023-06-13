f11e54b2097983b11bc40c0c27db37a678e0ff6a
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2023-06-13 07:17:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -55,4 +55,54 @@ bot.onText(/\/keyboard/, msg => {
     },
   });
 });
+
+const inline_keyboard = [
+  [
+    { text: 'Reply', callback_data: 'reply' },
+    { text: 'Forward', callback_data: 'forward' },
+  ],
+  [
+    { text: 'Edit', callback_data: 'edit' },
+    { text: 'Delete', callback_data: 'delete' },
+  ],
+];
+
+bot.onText(/\/start/, msg => {
+  const { id } = msg.chat;
+
+  bot.sendMessage(id, 'Выберите кнопку', {
+    reply_markup: {
+      inline_keyboard,
+    },
+  });
+});
+
+bot.on('callback_query', query => {
+  const { chat, message_id, text } = query.message;
+  switch (query.data) {
+    case 'forward':
+      // куда, откуда, что
+      bot.forwardMessage(chat.id, chat.id, message_id);
+      break;
+    case 'reply':
+      bot.sendMessage(chat.id, 'Отвечаем на сообщение', {
+        reply_to_message_id: message_id,
+      });
+      break;
+    case 'edit':
+      bot.editMessageText(`${text} (edited)`, {
+        chat_id: chat.id,
+        message_id: message_id,
+        reply_markup: {
+          inline_keyboard,
+        },
+      });
+      break;
+    case 'delete':
+      bot.deleteMessage(chat.id, message_id);
+  }
+
+  bot.answerCallbackQuery({ callback_query_id: query.id }, `${query.data}`);
+});
+
 bot.on('polling_error', console.log);
